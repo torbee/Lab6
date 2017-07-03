bd68e87fc3347a94a5909b1897a663861ab83e3b
torbee <vkishman88@gmail.com>
2017-07-03 14:26:30 -0400

    initial commit WIP

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Pig Latin Translator!");
+            Console.WriteLine("Enter a word to be translated: ");
+            string input = Console.ReadLine();
+            input = input.ToLower();
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' } ;
+            char[] inputArray = input.ToArray();
+            int length = inputArray.Length;
+            char c = input[0];
+            if (vowels.Contains(c))
+            {
+                Console.WriteLine(input + "way.");
+            }
+            else if(!vowels.Contains(c))
+            {
+                string sub = input.Substring(1, length);
+                Console.WriteLine(sub + c + "ay");
+                //sub plus index 0 plus ay
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid word.");
+            }
+            
+        }
+    }
+}
