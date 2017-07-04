f248969ce1dfc84902a0572f7d5c9741fe3a6576
torbee <vkishman88@gmail.com>
2017-07-04 15:35:55 -0400

    added symbol exception

--- Program.cs
+++ Program.cs
@@ -11,28 +11,61 @@ namespace Lab6
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Pig Latin Translator!");
-            Console.WriteLine("Enter a word to be translated: ");
-            string input = Console.ReadLine();
-            input = input.ToLower();
-            char[] vowels = { 'a', 'e', 'i', 'o', 'u' } ;
-            char[] inputArray = input.ToArray();
-            int length = inputArray.Length;
-            char c = input[0];
-            if (vowels.Contains(c))
+            bool run = true;
+            while (run == true)
             {
-                Console.WriteLine(input + "way.");
+                Console.WriteLine("Enter a word to be translated: ");
+                string input = Console.ReadLine();
+                input = input.ToLower();
+                char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+                char[] inputArray = input.ToArray();
+                int length = inputArray.Length;
+                char c = input[0];
+                Boolean nonAlpha = true;
+                foreach(var letter in input)
+                {
+                    if (Char.IsDigit(letter) || Char.IsPunctuation(letter) || Char.IsSymbol(letter))
+                    {
+                        nonAlpha = false;
+                        break;
+                    }
+                }
+                if (nonAlpha && vowels.Contains(c))
+                {
+                    Console.WriteLine($"Translation: {input}way.");
+                }
+                else if (nonAlpha && !vowels.Contains(c))
+                {
+                    string sub = input.Substring(1);
+                    Console.WriteLine($"Translation: {sub}{c}ay");
+                }
+                else if (!nonAlpha)
+                {
+                    Console.WriteLine($"Translation: {input}");
+                }
+
+                run = Continue();
             }
-            else if(!vowels.Contains(c))
-            {
-                string sub = input.Substring(1, length);
-                Console.WriteLine(sub + c + "ay");
-                //sub plus index 0 plus ay
+        }
+            public static Boolean Continue() {
+                Boolean run;
+                Console.WriteLine("Enter a new word? (y/n): ");
+                string answer = Console.ReadLine();
+
+                    if (answer == "y" || answer == "Y")
+                    {
+                        run = true;
+                    }
+                    else if (answer == "n" || answer == "N")
+                    {
+                        run = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Input not valid. Please try again.");
+                        run = Continue();
+                    }
+            return run;
             }
-            else
-            {
-                Console.WriteLine("Please enter a valid word.");
-            }
-            
         }
     }
-}
